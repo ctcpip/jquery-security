5b69a4d95d43ea3a7749adcb32bfd4543405046e
ctcpip <ctcpip@users.noreply.github.com>
2024-02-22 17:51:14 -0600

    🔥 remove commented dead code

--- security/server/index.cjs
+++ security/server/index.cjs
@@ -9,7 +9,6 @@ const chalk = require('chalk');
 xssApp.use(cors());
 
 xssApp.get('/jqueryxss', (req, res) => {
-	// res.set('content-type', 'text/javascript');
 	res.writeHead(200, { 'Content-Type': 'text/javascript' });
 	res.write(`triggerCVE('2015-9251');`)
 	res.end()
